e5de179154c39f382fa932fca2401687cc6e3c68
ahmedjilani123 <ahmedjilani0011@gmail.com>
2024-07-23 20:53:36 +0530

    change btn name

--- Portfolio/src/component/Navbar/FooterSideBar.jsx
+++ Portfolio/src/component/Navbar/FooterSideBar.jsx
@@ -6,7 +6,7 @@ const FooterSideBar = () => {
     <>
       <div className='h-full border border-orange-600 overflow-y-hidden mt-5 rounded-sm p-2'>
         <div className='flex justify-between mt-2  w-full rounded-lg'>
-            <button className='bg-orange-600 text-custom-dark-blue w-full rounded-l-lg h-9 active:bg-slate-100'>Download CV</button>
+            <button className='bg-orange-600 text-custom-dark-blue w-full rounded-l-lg h-9 active:bg-slate-100'>Click CV</button>
             <button className='bg-custom-dark-blue text-orange-600 w-full rounded-r-lg h-9 border border-orange-600 active:bg-orange-500'>Contact</button>
         </div>
         <div>
